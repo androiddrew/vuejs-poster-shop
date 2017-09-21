550135d831e0a3a47733632e25a0d6eab4b14fc4
AndroidDrew <bednar.andrew@gmail.com>
2017-09-21 16:52:18 -0400

    Added base Vue app

--- public/script.js
+++ public/script.js
@@ -1,1 +1,34 @@
-console.log('It works.');
+//We use const here because we don't want the assignment of the root app to change
+const app = new Vue({
+  el: "#app",
+  data:{
+    total: 0.00,
+    items: [
+      {title: 'Item 1'},
+      {title: 'Item 2'},
+      {title: 'Item 3'}
+    ],
+    cart:[],
+    seen: true
+  },
+  methods:{
+    addItem: function(index){
+
+      let item = this.items[index];
+
+      for (record of this.cart){
+        if (record.title == item.title){
+          record.qty += 1;
+          this.total += 9.99;
+          return;
+        }
+      }
+
+      this.cart.push({
+        title: item.title,
+        qty: 1
+      });
+      this.total += 9.99;
+    }
+  }
+});
